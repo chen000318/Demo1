38059b9ba67b456008b237be08a644ce67dedff7
liuguangxin <3304375706@qq.com>
2022-11-28 20:29:46 +0800

    提交Dao添加，修改，删除，登录；Biz添加，修改，删除，登录；index.jsp,register.jsp

--- src/user/biz/impl/UserBizImpl.java
+++ src/user/biz/impl/UserBizImpl.java
@@ -1,19 +1,25 @@
 package user.biz.impl;
 
 import user.biz.IUserBiz;
+import user.dao.impl.UserDaoImpl;
 import user.entry.Emp;
 
 import java.util.List;
 
 public class UserBizImpl implements IUserBiz {
+
+    UserDaoImpl userDao = new UserDaoImpl();
+
     @Override
     public boolean rigister(Emp emp) {
-        return false;
+
+        return userDao.insert(emp)==1?true:false;
     }
 
     @Override
     public boolean login(Emp emp) {
-        return false;
+
+        return userDao.verifyingLogin(emp)==1?true:false;
     }
 
     @Override
@@ -23,12 +29,14 @@ public class UserBizImpl implements IUserBiz {
 
     @Override
     public boolean delete(Emp emp) {
-        return false;
+
+        return userDao.delete(emp)==1?true:false;
     }
 
     @Override
     public boolean updateEmp(Emp emp) {
-        return false;
+
+        return userDao.update(emp)==1?true:false;
     }
 
     @Override
--- web/index.jsp
+++ web/index.jsp
@@ -8,9 +8,22 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>登录界面</title>
+    <script type="text/javascript">
+      function show() {
+        var boo = window.confirm("确定");
+        return boo;
+      }
+    </script>
   </head>
   <body>
-  $END$
+  <div align="center">
+    <form action="ss" method="post">
+      员工编号: <input type="text" name="empno"><br/>
+      员工姓名: <input type="text" name="ename"><br/>
+               <input type="submit" value="登录" ><br/>
+                <a href="register.jsp"><input type="submit" value="注册" onclick="return show()"></a>
+    </form>
+  </div>
   </body>
 </html>
